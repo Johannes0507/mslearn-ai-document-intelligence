f61420a61aeeb0592cfc89b3513522fb9f6bee11
Keith <keith.lee@diamondgroup.com.tw>
2025-06-15 22:47:44 +0800

    Learning

--- Labfiles/01-prebuild-models/CSharp/Program.cs
+++ Labfiles/01-prebuild-models/CSharp/Program.cs
@@ -2,8 +2,8 @@
 using Azure.AI.FormRecognizer.DocumentAnalysis;
 
 // Store connection information
-string endpoint = "<Endpoint URL>";
-string apiKey = "<API Key>";
+string endpoint = "https://learning-docuemnt-intelligence.cognitiveservices.azure.com/";
+string apiKey = "D4Smb6onpETjUivJOB4zfRU4yBqzhVPvzatvSb6qrP32oVcAuTSWJQQJ99BFACYeBjFXJ3w3AAALACOGHwGT";
 
 Uri fileUri = new Uri("https://github.com/MicrosoftLearning/mslearn-ai-document-intelligence/blob/main/Labfiles/01-prebuild-models/sample-invoice/sample-invoice.pdf?raw=true");
 
@@ -11,15 +11,29 @@ Console.WriteLine("\nConnecting to Forms Recognizer at: {0}", endpoint);
 Console.WriteLine("Analyzing invoice at: {0}\n", fileUri.ToString());
 
 // Create the client
+var azureCardential = new AzureKeyCredential(apiKey);
+var client = new DocumentAnalysisClient(new Uri(endpoint), azureCardential);
 
 // Analyze the invoice
+AnalyzeDocumentOperation operation = await client.AnalyzeDocumentFromUriAsync(
+    WaitUntil.Completed,
+    "prebuilt-invoice",
+    fileUri);
 
 // Display invoice information to the user
+AnalyzeResult result = operation.Value;
 
-
-
-
-
+foreach (AnalyzedDocument invoice in result.Documents)
+{
+    if (invoice.Fields.TryGetValue("VendorName", out DocumentField? vendorNameField))
+    {
+        if (vendorNameField.FieldType == DocumentFieldType.String)
+        {
+            string vendorName = vendorNameField.Value.AsString();
+            Console.WriteLine($"Vendor Name: '{vendorName}', with confidence {vendorNameField.Confidence}.");
+        }
+    }
+    
     if (invoice.Fields.TryGetValue("CustomerName", out DocumentField? customerNameField))
     {
         if (customerNameField.FieldType == DocumentFieldType.String)
@@ -38,4 +52,5 @@ Console.WriteLine("Analyzing invoice at: {0}\n", fileUri.ToString());
         }
     }
 }
+
 Console.WriteLine("\nAnalysis complete.\n");
